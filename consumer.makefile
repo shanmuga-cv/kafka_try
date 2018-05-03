3f4dfd806f3c5270e0150fb59077991c1468b346
Shanmuga <shanmuga.chidambaravel@gmail.com>
2018-05-03 14:25:40 +0200

    java consumer

--- consumer.makefile
+++ consumer.makefile
@@ -1,6 +1,16 @@
-kafka_home := ~/kafka_2.11-1.1.0
+kafka_home := /opt/app-root/src/kafka_2.11-1.1.0
 
-# Consumer
+# Java consumer
+run_consumer: compile_consumer
+	java -cp "bin:$(kafka_home)/libs/*" consumer.SimpleConsumer
+
+compile_consumer: clean_consumer
+	javac consumer/*.java -cp "$(kafka_home)/libs/*" -d bin
+
+clean_consumer:
+	rm -rf bin/consumer
+
+# Console consumer
 consoler_consumer: 
 	cd $(kafka_home) && \
 	bin/kafka-console-consumer.sh --bootstrap-server kafka_server_1:9092 --topic my_topic #--from-beginning
---
+++ consumer/SimpleConsumer.java
@@ -0,0 +1,32 @@
+package consumer;
+
+import java.util.Properties;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+
+public class SimpleConsumer {
+    public static void main(String []args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "kafka_server_1:9092");
+        props.put("group.id", "test");
+        props.put("enable.auto.commit", "false");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("my_topic"));
+        final int minBatchSize = 200;
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.println(record);
+            }
+            consumer.commitSync();
+        }
+    }
+}
