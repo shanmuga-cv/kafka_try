e25ac592767fd2e6bb4185ce5854ba0b01313230
Shanmuga <shanmuga.chidambaravel@gmail.com>
2018-05-03 17:13:48 +0200

    added client.id and group.id in consumer

--- consumer/SimpleConsumer.java
+++ consumer/SimpleConsumer.java
@@ -13,7 +13,8 @@ public class SimpleConsumer {
     public static void main(String []args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "kafka_server_1:9092");
-        props.put("group.id", "test");
+        props.put("group.id", "first_group");
+        props.put("client.id", "consumer_1");
         props.put("enable.auto.commit", "false");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
