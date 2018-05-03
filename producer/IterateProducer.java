398843670d64b1e562e9839eedfdcc78ee8b318e
Shanmuga <shanmuga.chidambaravel@gmail.com>
2018-05-03 18:19:34 +0200

    IterateProducer with partitioner

--- producer/IterateProducer.java
+++ producer/IterateProducer.java
@@ -11,27 +11,32 @@ public class IterateProducer {
     public static void main(String args[]) throws InterruptedException {
         int sleep_time = Integer.parseInt(args[0]);
         int no_messages = Integer.parseInt(args[1]);
-        String msg_prefix = args[2];
+        String message_prefix = args[2];
+        String topic = "my_topic";
 
         Properties props = new Properties();
         props.put("bootstrap.servers", "kafka_server_1:9092");
         props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
         props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String, String> producer = new KafkaProducer<>(props);
+        int no_partitions = producer.partitionsFor(topic).size();
         for(int i = 0; i < no_messages; i++) {
             Thread.sleep(sleep_time);
-            String message = String.format("%s %d", msg_prefix, i);
-            ProducerRecord<String, String> record = new ProducerRecord<String, String>("my_topic", Integer.toString(i), message);
+            ProducerRecord<String, String> record = makeRecord("my_topic", i, message_prefix, no_partitions);
             System.out.println("sending msg " + record);
             producer.send(record);
         }
 
         producer.close();
+    }
+
+    static ProducerRecord<String, String> makeRecord(String topic, int no, String message_prefix, int no_partitions) {
+        int partition = no % no_partitions;
+        String key = Integer.toString(no);
+        String value = String.format("%s %d", message_prefix, no);
+        return new ProducerRecord<String, String>(topic, partition, key, value);
     }
 }
