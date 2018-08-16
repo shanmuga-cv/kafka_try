d0535b8eb6974da702b7942bc7e4cbf496b611e8
Shanmuga <shanmuga.chidambaravel@gmail.com>
2018-08-16 18:13:54 +0200

    updated kafka host used in docker-compose and refactred

--- consumer/SimpleConsumer.java
+++ consumer/SimpleConsumer.java
@@ -10,22 +10,21 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 
-
-
 public class SimpleConsumer {
     public static void main(String []args) {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "kafk_spark_server_1:9092");
+        props.put("bootstrap.servers", "dev_kafka_server_1:9092");
         props.put("group.id", "first_group");
         props.put("client.id", "consumer_1");
         props.put("enable.auto.commit", "false");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("auto.offset.reset", "earliest");
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("spark_kafka_events"));
-        final int minBatchSize = 200;
-        while (true) {
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+        List<String> topics = Arrays.asList("spark_kafka_events");
+        consumer.subscribe(topics);
+
+         while(true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
             System.out.println("done " + records.count());
             for (ConsumerRecord<String, String> record : records) {
