b127bd4ae9bdef2ee4fd6520b553f9b376a1adab
Shanmuga <shanmuga.chidambaravel@gmail.com>
2019-06-17 12:22:09 +0200

    Added range consumer

---
+++ consumer/RangeConsumer.scala
@@ -0,0 +1,55 @@
+
+import java.util.Properties
+
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.consumer.ConsumerRecord
+
+import scala.collection.JavaConverters._
+
+
+class RangeConsumer(val kafkaProps: Properties, topics: scala.List[String], maxOffsets: Map[Int, Long]) extends Iterator[ConsumerRecord[String, String]] {
+  private val consumer = new KafkaConsumer[String, String](kafkaProps);
+  consumer.subscribe(topics.asJava)
+  val maxRetries = 10
+  private val pollDuration = 1000L
+  var isFinished: Boolean = false
+
+  var records: scala.Iterator[ConsumerRecord[String, String]] = scala.Iterator.empty
+
+  private def poll(): Unit = {
+//    records = consumer.poll(pollDuration).iterator().asScala.filter(filter).toList
+    val consumerRecords = consumer.poll(pollDuration)
+    consumerRecords.count()
+    records = consumerRecords.iterator().asScala.filter(filter)
+    consumer.commitAsync()
+  }
+
+  def filter(consumerRecord: ConsumerRecord[String, String]): Boolean = {
+    consumerRecord.offset() <= maxOffsets(consumerRecord.partition())
+  }
+
+  override def hasNext(): Boolean = {
+    var retryCount = maxRetries
+    while (retryCount > 0) {
+      if (records.hasNext)
+        return true
+      else {
+        poll()
+        retryCount -= 1
+      }
+    }
+    false
+  }
+
+  override def next(): ConsumerRecord[String, String] = {
+    if (hasNext()) {
+      val record = records.next()
+      return record
+    } else {
+      consumer.close()
+      throw new Exception("didn't get any records")
+    }
+  }
+
+  override def toString: String = f"${this.getClass.getName}<${this.topics}, ${this.maxOffsets}>"
+}
